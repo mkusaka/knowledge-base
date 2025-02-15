2bd019cf40f283b28bd0460b65bb420e5d839b4f
mkusaka <hinoshita1992@gmail.com>
2025-02-16 01:01:07 +0900

    tidy up

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'drizzle-kit'
-export default defineConfig({
-  schema: './src/db/schema.ts',
-  dialect: "sqlite",
-  out: './drizzle',
-  dbCredentials: {
-    url: 'file:./crawler.db'
-  }
-})
